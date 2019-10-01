7e0174f365425aa8e998bdaf19b24e11b926a58e
Rob Baltzer <rob.baltzer@iotashome.com>
2019-10-01 13:37:51 -0700

    First pass at UI

--- ble-relay/ContentView.swift
+++ ble-relay/ContentView.swift
@@ -10,12 +10,58 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello World")
+    
+        NavigationView {
+            FormView()
+        }
     }
 }
 
+struct FormView : View {
+    var body: some View {
+        
+        Form {
+            Section {
+                HStack {
+                    Text("Value:")
+                    Text("444") // TODO: Put relayed value here
+                }
+            }
+            
+            Section {
+                Button(action: {
+                // TODO: Start sending packets, toggle button
+                }) { Text("Start")}
+            }
+            
+            Section {
+                HStack {
+                    Text("Status:")
+                    Text("Connected")
+                }
+            }
+            
+            Section {
+                NavigationLink(destination: Log()) {
+                    // TODO: put log of BLE events here
+                    Text("View log")
+
+                }
+            }
+        }.navigationBarTitle(Text("BLE Relay"))
+    }
+}
+
+struct Log : View {
+    var body: some View {
+        Text("LOG HERE TBD")
+    }
+}
+
+#if DEBUG
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+#endif
