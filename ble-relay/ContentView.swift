8a9b084bc94f4c6b43703abedf70766a1724ae4f
Rob Baltzer <rob.baltzer@iotashome.com>
2019-10-01 09:50:44 -0700

    Initial commit

---
+++ ble-relay/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ble-relay
+//
+//  Created by Robert Baltzer on 10/1/19.
+//  Copyright © 2019 Robert Baltzer. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
